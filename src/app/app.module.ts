c7bb41e467968a4681f7a0b7810f3f84b7a09c98
Maria Stezhko <stezhko@gmail.com>
2018-02-19 15:34:52 -0800

    feat: Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpService } from './http.service';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SeattleComponentComponent } from './seattle-component/seattle-component.component';
+import { SeattleComponent } from './seattle/seattle.component';
+import { SanJoseComponent } from './san-jose/san-jose.component';
+import { BurbankComponent } from './burbank/burbank.component';
+import { DallasComponent } from './dallas/dallas.component';
+import { WashingtonDcComponent } from './washington-dc/washington-dc.component';
+import { ChicagoComponent } from './chicago/chicago.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeattleComponentComponent,
+    SeattleComponent,
+    SanJoseComponent,
+    BurbankComponent,
+    DallasComponent,
+    WashingtonDcComponent,
+    ChicagoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/seattle-component/seattle-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seattle-component',
+  templateUrl: './seattle-component.component.html',
+  styleUrls: ['./seattle-component.component.css']
+})
+export class SeattleComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
